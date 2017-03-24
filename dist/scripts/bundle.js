c3cd451bad83787806c23cef0bbd2b33789f9b4f
Jonathan Dann <jdann@rei.com>
2017-03-24 14:21:06 -0700

    transitions

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -49234,7 +49234,24 @@ module.exports = {
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
-   render: function(){
+
+    statics: {
+        willTransitionTo: function(transition, params, query, callback){
+
+            if (confirm('Are you sure you want to visit this page?')){
+                callback();
+            } else {
+                transition.abort();
+            }
+        },
+        willTransitionFrom: function(transition, component){
+
+            if (!confirm('Are you sure you want to leave this page?')){
+                transition.abort();
+            }
+        }
+    },
+    render: function(){
        return (
            React.createElement("div", null, 
                React.createElement("h1", null, "About"), 
--- src/components/about/about-page.js
+++ src/components/about/about-page.js
@@ -3,7 +3,24 @@
 var React = require('react');
 
 var About = React.createClass({
-   render: function(){
+
+    statics: {
+        willTransitionTo: function(transition, params, query, callback){
+
+            if (confirm('Are you sure you want to visit this page?')){
+                callback();
+            } else {
+                transition.abort();
+            }
+        },
+        willTransitionFrom: function(transition, component){
+
+            if (!confirm('Are you sure you want to leave this page?')){
+                transition.abort();
+            }
+        }
+    },
+    render: function(){
        return (
            <div>
                <h1>About</h1>
