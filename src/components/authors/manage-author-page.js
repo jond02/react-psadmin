7bb7b505ad5bdc27c5168a2a95801a7ffb7f6e9c
Jonathan Dann <nwjon122@gmail.coml>
2017-03-25 11:38:11 -0700

    form notification and validation

--- src/components/authors/manage-author-page.js
+++ src/components/authors/manage-author-page.js
@@ -1,16 +1,24 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./author-form');
+var AuthorApi = require('../../api/author-api');
+var toastr = require('toastr');
 
 //controller view
 //top level components are best place to handle state
 var ManageAuthorPage = React.createClass({
 
+    mixins: [
+        Router.Navigation
+    ],
+
     getInitialState: function(){
 
         return {
-            author: {id: '', firstName: '', lastName: ''}
+            author: {id: '', firstName: '', lastName: ''},
+            errors: {}
         };
     },
     setAuthorState: function(event){
@@ -21,12 +29,42 @@ var ManageAuthorPage = React.createClass({
         this.state.author[field] = value;
         return this.setState({author: this.state.author});
     },
+    authorFormIsValid: function(){
+
+        var formIsValid = true;
+        this.state.errors = {}; //clear any previous errors
+
+        if (this.state.author.firstName.length < 2){
+            this.state.errors.firstName = 'First name must be at least 2 characters';
+            formIsValid = false;
+        }
+        if (this.state.author.lastName.length < 2){
+            this.state.errors.lastName = 'Last name must be at least 2 characters';
+            formIsValid = false;
+        }
+        //any time change state need to call setState
+        this.setState({errors: this.state.errors});
+        return formIsValid;
+    },
+    saveAuthor: function(event){
+        event.preventDefault();
+
+        if (!this.authorFormIsValid()){
+            return;
+        }
+
+        AuthorApi.saveAuthor(this.state.author);
+        toastr.success('Author saved.');
+        this.transitionTo('authors');
+    },
     render: function(){
 
         return (
             <AuthorForm
                 author={this.state.author}
                 onChange={this.setAuthorState}
+                onSave={this.saveAuthor}
+                errors={this.state.errors}
             />
         );
     }
